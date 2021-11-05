3569fed26261ebb247d388508ee631b6fe1dd2c4
caiser666 <chailirsentosa3696@gmail.com>
2021-11-05 22:56:28 +0700

    Add Handling delete item and update myListPokemon

--- app/src/main/java/com/example/pokemon/adapters/MyPokemonListAdapter.kt
+++ app/src/main/java/com/example/pokemon/adapters/MyPokemonListAdapter.kt
@@ -8,19 +8,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.pokemon.R
 import com.example.pokemon.models.PokemonDetailResponse
-import kotlinx.android.synthetic.main.card_item_pokemon.view.*
+import com.example.pokemon.utils.MySharedPref
+import kotlinx.android.synthetic.main.card_item_my_list_pokemon.view.*
+import kotlinx.android.synthetic.main.card_item_pokemon.view.iv_pokemon_image
+import kotlinx.android.synthetic.main.card_item_pokemon.view.tv_pokemon_name
 
 class MyPokemonListAdapter(val pokemonClick: (Int) -> Unit): RecyclerView.Adapter<MyPokemonListAdapter.SearchViewHolder>() {
-    var pokemonList: List<PokemonDetailResponse> = emptyList()
+    private lateinit var pokemonList: MutableList<PokemonDetailResponse>
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setData(list: List<PokemonDetailResponse>){
+    fun setData(list: MutableList<PokemonDetailResponse>){
         pokemonList = list
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        return SearchViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_item_pokemon, parent,false))
+        MySharedPref.init(parent.context)
+        return SearchViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_item_my_list_pokemon, parent,false))
+
     }
 
     override fun getItemCount(): Int {
@@ -33,6 +38,17 @@ class MyPokemonListAdapter(val pokemonClick: (Int) -> Unit): RecyclerView.Adapte
         holder.itemView.setOnClickListener { pokemonClick(pokemon.id) }
         val imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
         Glide.with(holder.itemView.context).load(imgUrl).into(holder.itemView.iv_pokemon_image)
+
+        holder.itemView.ibtn_remove_pokemon.setOnClickListener {
+            deleteItem(position)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun deleteItem(index: Int) {
+        pokemonList.removeAt(index)
+        MySharedPref.myPokemonList = pokemonList
+        notifyDataSetChanged()
     }
 
     class SearchViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
